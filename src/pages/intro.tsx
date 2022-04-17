3a6f395f4f0717523a1c4574fb2551afd13ed6c3
Jalen Moore <jalenmoore@unomaha.edu>
2022-04-17 13:02:22 -0500

    Made a simple hello world

---
+++ src/pages/intro.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Test() {
+    const element = (
+        <div>
+            <p>
+                Hello, World!
+            </p>
+        </div>
+    );
+    return element;
+}
